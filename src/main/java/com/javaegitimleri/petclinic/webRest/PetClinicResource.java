8ec3c3574c9939049fa1f904379c2c3c8839c64b
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-03-07 09:54:59 +0300

    PetClinic-037-JSP-dinamic-page JSP dynamic pages created

--- src/main/java/com/javaegitimleri/petclinic/webRest/PetClinicResource.java
+++ src/main/java/com/javaegitimleri/petclinic/webRest/PetClinicResource.java
@@ -1,13 +1,27 @@
 package com.javaegitimleri.petclinic.webRest;
 
+import com.javaegitimleri.petclinic.service.petclinic.PetClinicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PetClinicResource {
 
-    @GetMapping("/welcome")
+    @Autowired
+    PetClinicService petClinicService;
+
+    @RequestMapping("/owners")
+    public ModelAndView getOwners() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("owners", petClinicService.findOwners());
+        modelAndView.setViewName("owners");
+        return modelAndView;
+    }
+
+    @RequestMapping("/welcome")
     @ResponseBody
     public String welcome() {
         return "Welcome to PetClinic World!";
