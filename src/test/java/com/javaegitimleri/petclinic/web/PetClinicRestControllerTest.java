31e458a9aa38a7c57d6f5f30dec277ced81a54e9
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-03-08 10:17:29 +0300

    PetClinic-053-rest-api-II Sample Rest Api new test methods created at PetClinicRestControllerTest

--- src/test/java/com/javaegitimleri/petclinic/web/PetClinicRestControllerTest.java
+++ src/test/java/com/javaegitimleri/petclinic/web/PetClinicRestControllerTest.java
@@ -8,6 +8,10 @@ import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class PetClinicRestControllerTest {
 
     private RestTemplate restTemplate;
@@ -22,5 +26,23 @@ public class PetClinicRestControllerTest {
         ResponseEntity<Owner> response = restTemplate.getForEntity("http://localhost:8082/rest/owner/1", Owner.class);
         MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
         MatcherAssert.assertThat(response.getBody().getFirstName(), Matchers.equalTo("John"));
+    }
+
+    @Test
+    public void testGetOwnersByLastName() {
+        ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:8082/rest/owners-by-last-name?lastName=Doe", List.class);
+        MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
+        List<Map<String, String>> body = response.getBody();
+        List<String> firstNameOfOwners = body.stream().map(item -> item.get("firstName")).collect(Collectors.toList());
+        MatcherAssert.assertThat(firstNameOfOwners, Matchers.containsInAnyOrder("Charles", "Susan", "John", "Jane"));
+    }
+
+    @Test
+    public void testGetOwner() {
+        ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:8082/rest/owners", List.class);
+        MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
+        List<Map<String, String>> body = response.getBody();
+        List<String> firstNameOfOwners = body.stream().map(item -> item.get("firstName")).collect(Collectors.toList());
+        MatcherAssert.assertThat(firstNameOfOwners, Matchers.containsInAnyOrder("Charles", "Susan", "John", "Jane"));
     }
 }
