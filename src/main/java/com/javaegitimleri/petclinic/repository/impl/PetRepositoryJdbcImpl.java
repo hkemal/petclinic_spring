39f9e4dc7ed1c0f215a3fd58f3771b3b85a0c746
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-03-11 15:52:48 +0300

    PetClinic-076-transaction-management delete method rollback proporties tested

---
+++ src/main/java/com/javaegitimleri/petclinic/repository/impl/PetRepositoryJdbcImpl.java
@@ -0,0 +1,52 @@
+package com.javaegitimleri.petclinic.repository.impl;
+
+import com.javaegitimleri.petclinic.entity.Pet;
+import com.javaegitimleri.petclinic.repository.repo.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("petRepository")
+public class PetRepositoryJdbcImpl implements PetRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Pet> findAll() {
+        return null;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Pet> findByOwnerId(Long ownerId) {
+        return null;
+    }
+
+    @Override
+    public void create(Pet pet) {
+
+    }
+
+    @Override
+    public Pet update(Pet pet) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public void deleteByOwnerId(Long ownerId) {
+        String sql = " delete from t_pet where owner_id = ?";
+        jdbcTemplate.update(sql, ownerId);
+    }
+}
