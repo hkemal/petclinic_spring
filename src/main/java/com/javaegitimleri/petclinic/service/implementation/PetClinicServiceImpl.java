e08890e5e5ded02d795a44e0f634d826901fb337
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-03-13 14:17:11 +0300

    PetClinic-084-tx-settings transaction setting changed

--- src/main/java/com/javaegitimleri/petclinic/service/implementation/PetClinicServiceImpl.java
+++ src/main/java/com/javaegitimleri/petclinic/service/implementation/PetClinicServiceImpl.java
@@ -7,12 +7,13 @@ import com.javaegitimleri.petclinic.repository.repo.PetRepository;
 import com.javaegitimleri.petclinic.service.petclinic.PetClinicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(rollbackFor = Exception.class)
 public class PetClinicServiceImpl implements PetClinicService {
 
     @Autowired
@@ -22,16 +23,19 @@ public class PetClinicServiceImpl implements PetClinicService {
     PetRepository petRepository;
 
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
     public List<Owner> findOwners() {
         return ownerRepository.findAll();
     }
 
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
     public List<Owner> findOwners(String lastName) {
         return ownerRepository.findByLastName(lastName);
     }
 
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
     public Owner findOwner(Long id) throws OwnersNotFoundException {
         Owner owner = ownerRepository.findById(id);
         if (owner == null) throw new OwnersNotFoundException("Owner not found with id : " + id);
