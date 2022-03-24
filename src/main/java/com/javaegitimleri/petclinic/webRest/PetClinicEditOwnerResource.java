7060c98d8d856d4eaaff787fb25bcae02dc40d8b
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-03-24 18:04:54 +0300

    PetClinic-121-update-owner update api created

---
+++ src/main/java/com/javaegitimleri/petclinic/webRest/PetClinicEditOwnerResource.java
@@ -0,0 +1,31 @@
+package com.javaegitimleri.petclinic.webRest;
+
+import com.javaegitimleri.petclinic.entity.Owner;
+import com.javaegitimleri.petclinic.service.petclinic.PetClinicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PetClinicEditOwnerResource {
+
+    @Autowired
+    PetClinicService petClinicService;
+
+    @RequestMapping(value = "/owners/update/{id}", method = RequestMethod.GET)
+    public String loadOwner(@PathVariable Long id, ModelMap modelMap) {
+        Owner owner = petClinicService.findOwner(id);
+        modelMap.put("owner", owner);
+        return "editOwner";
+    }
+
+    @RequestMapping(value = "/owners/update/{id}", method = RequestMethod.POST)
+    public String handleFormSubmit(@ModelAttribute Owner owner) {
+        petClinicService.updateOwner(owner);
+        return "redirect:/owners";
+    }
+}
